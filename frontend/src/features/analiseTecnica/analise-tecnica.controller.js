7e2febc89e409e94b7f6e4c726bc35bfc437e669
Cleiton Luiz <cleiton.teodoro@gt4w.com.br>
2017-06-21 15:43:05 -0300

    Criação da interface de parece técnico

--- frontend/src/features/analiseTecnica/analise-tecnica.controller.js
+++ frontend/src/features/analiseTecnica/analise-tecnica.controller.js
@@ -1,8 +1,34 @@
 var AnaliseTecnicaController = function($rootScope, $scope, $routeParams, $window, $location, 
         analiseTecnica, documentoLicenciamentoService, uploadService, mensagem, $uibModal, analiseTecnicaService, documentoAnaliseService, processoService) {    
 
+    var ctrl = this;
+    
     $rootScope.tituloPagina = 'PARECER Técnico';
-    alert('a');
+
+    ctrl.processo = angular.copy(analiseTecnica.analise.processo);
+
+
+    ctrl.exibirDadosProcesso = exibirDadosProcesso;
+    
+    function exibirDadosProcesso() {
+
+        var processo = {
+
+            idProcesso: ctrl.processo.id,
+            numero: ctrl.processo.numero,
+            denominacaoEmpreendimento: ctrl.processo.empreendimento.denominacao
+        };
+
+        if(ctrl.processo.empreendimento.pessoa.cnpj) {
+
+            processo.cnpjEmpreendimento = ctrl.processo.empreendimento.pessoa.cnpj;
+
+        } else {
+
+            processo.cpfEmpreendimento = ctrl.processo.empreendimento.pessoa.cpf;
+        }
+        processoService.visualizarProcesso(processo);
+    }
 };
 
 exports.controllers.AnaliseTecnicaController = AnaliseTecnicaController;
