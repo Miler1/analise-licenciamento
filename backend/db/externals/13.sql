9d7f73efc0b7c3135a369a5105dbf1d5f9d6a116
Cleiton Luiz <cleiton.teodoro@gt4w.com.br>
2017-06-22 16:29:03 -0300

    Correção na descrição da permissão

--- backend/db/externals/13.sql
+++ backend/db/externals/13.sql
@@ -5,7 +5,7 @@ DELETE FROM portal_seguranca.permissao_perfil WHERE id_perfil = 8 AND id_permiss
 INSERT INTO portal_seguranca.permissao_perfil(id_perfil, id_permissao)
 VALUES (5, (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'INICIAR_PARECER_JURIDICO'));
 
-INSERT INTO portal_seguranca.permissao (codigo, data_cadastro, nome, id_modulo) VALUES ('INICIAR_PARECER_TECNICO', NOW(), 'Vincular esta permissão ao perfil Analista técnico', 5);
+INSERT INTO portal_seguranca.permissao (codigo, data_cadastro, nome, id_modulo) VALUES ('INICIAR_PARECER_TECNICO', NOW(), 'niciar/continuar parecer da análise técnica', 5);
 
 # ---!Downs
 
