7fafad9279570565bf5b21357f3172e04b2faba4
Cleiton Luiz <cleiton.teodoro@gt4w.com.br>
2017-06-22 16:40:11 -0300

    Correção da evolution 13

--- backend/db/externals/13.sql
+++ backend/db/externals/13.sql
@@ -2,10 +2,12 @@
 
 DELETE FROM portal_seguranca.permissao_perfil WHERE id_perfil = 8 AND id_permissao = (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'INICIAR_PARECER_JURIDICO');
 
-INSERT INTO portal_seguranca.permissao_perfil(id_perfil, id_permissao) VALUES (5, (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'INICIAR_PARECER_JURIDICO'));
+INSERT INTO portal_seguranca.permissao_perfil(id_perfil, id_permissao)
+VALUES (5, (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'INICIAR_PARECER_JURIDICO'));
+
+INSERT INTO portal_seguranca.permissao (codigo, data_cadastro, nome, id_modulo) VALUES ('INICIAR_PARECER_TECNICO', NOW(), 'Iniciar/continuar parecer da análise técnica', 5);
 
 INSERT INTO portal_seguranca.permissao_perfil(id_perfil, id_permissao) VALUES (8, (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'INICIAR_PARECER_TECNICO'));
-
 
 # ---!Downs
 
@@ -14,3 +16,5 @@ DELETE FROM portal_seguranca.permissao_perfil WHERE id_perfil = 8 AND  id_permis
 DELETE FROM portal_seguranca.permissao_perfil WHERE id_perfil = 5 AND  id_permissao IN (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'INICIAR_PARECER_JURIDICO');
 
 INSERT INTO portal_seguranca.permissao_perfil(id_perfil, id_permissao) VALUES (8, (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'INICIAR_PARECER_JURIDICO'));
+
+DELETE FROM portal_seguranca.permissao_perfil WHERE id_perfil = 8 AND id_permissao = (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'INICIAR_PARECER_TECNICO');
