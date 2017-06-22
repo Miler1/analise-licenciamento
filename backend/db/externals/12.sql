fc0d7690a6d911a57b60e1b0422672e8647ad8e4
Fernanda Possato <fernanda.possato@lemaf.ufla.br>
2017-06-22 12:03:08 -0300

    Grants para o usuário  do analise no esquema tramitacao.

--- backend/db/externals/12.sql
+++ backend/db/externals/12.sql
@@ -5,9 +5,19 @@ GRANT USAGE ON SCHEMA tramitacao TO licenciamento_pa;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA tramitacao TO licenciamento_pa;
 GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA tramitacao TO licenciamento_pa;
 
+GRANT USAGE ON SCHEMA analise TO licenciamento_pa;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA analise TO licenciamento_pa;
+GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA analise TO licenciamento_pa;
+
 # ---!Downs
 
 REVOKE USAGE ON SCHEMA tramitacao FROM licenciamento_pa;
 
 REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA tramitacao FROM licenciamento_pa;
 REVOKE SELECT, USAGE ON ALL SEQUENCES IN SCHEMA tramitacao FROM licenciamento_pa;
+
+REVOKE USAGE ON SCHEMA analise TO licenciamento_pa;
+
+REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA analise FROM licenciamento_pa;
+REVOKE SELECT, USAGE ON ALL SEQUENCES IN SCHEMA analise FROM licenciamento_pa;
