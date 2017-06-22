41f33bd88f889c256aa3026bd1695cee59594594
Cleiton Luiz <cleiton.teodoro@gt4w.com.br>
2017-06-22 16:07:13 -0300

    Correção do metodo para clonar parecer técnico

--- frontend/src/features/analiseTecnica/analise-tecnica.controller.js
+++ frontend/src/features/analiseTecnica/analise-tecnica.controller.js
@@ -10,65 +10,91 @@ var AnaliseTecnicaController = function ($rootScope, $scope, $routeParams, $wind
     ctrl.concluir = concluir;
     ctrl.salvar = salvar;
     ctrl.cancelar = cancelar;
-    
+
     ctrl.init = function () {
-        
+
         ctrl.analiseTecnica = angular.copy(analiseTecnica);
-        ctrl.analiseTecnica.analise.processo.empreendimento = null;
     };
 
-    function exibirDadosProcesso() {
+    function analiseValida() {
 
-        var processo = {
+        ctrl.formularioParecer.$setSubmitted();
+        ctrl.formularioResultado.$setSubmitted();
 
-            idProcesso: ctrl.processo.id,
-            numero: ctrl.processo.numero,
-            denominacaoEmpreendimento: ctrl.processo.empreendimento.denominacao
-        };
+        var parecerPreenchido = ctrl.formularioParecer.$valid;
+        var resultadoPreenchido = ctrl.formularioResultado.$valid;
+        var todosDocumentosValidados = true;
+        var todosDocumentosAvaliados = true;
 
-        if (ctrl.processo.empreendimento.pessoa.cnpj) {
+        ctrl.analiseTecnica.analisesDocumentos.forEach(function (analise) {
 
-            processo.cnpjEmpreendimento = ctrl.processo.empreendimento.pessoa.cnpj;
+            todosDocumentosAvaliados = todosDocumentosAvaliados && (analise.validado === true || (analise.validado === false && analise.parecer));
+            todosDocumentosValidados = todosDocumentosValidados && analise.validado;
+        });
+
+        if (ctrl.analiseJuridica.tipoResultadoAnalise.id === ctrl.DEFERIDO) {
+
+            return parecerPreenchido && todosDocumentosAvaliados && todosDocumentosValidados && resultadoPreenchido;
 
         } else {
 
-            processo.cpfEmpreendimento = ctrl.processo.empreendimento.pessoa.cpf;
+            return parecerPreenchido && todosDocumentosAvaliados && resultadoPreenchido;
         }
-        processoService.visualizarProcesso(processo);
+
+        function exibirDadosProcesso() {
+
+            var processo = {
+
+                idProcesso: ctrl.processo.id,
+                numero: ctrl.processo.numero,
+                denominacaoEmpreendimento: ctrl.processo.empreendimento.denominacao
+            };
+
+            if (ctrl.processo.empreendimento.pessoa.cnpj) {
+
+                processo.cnpjEmpreendimento = ctrl.processo.empreendimento.pessoa.cnpj;
+
+            } else {
+
+                processo.cpfEmpreendimento = ctrl.processo.empreendimento.pessoa.cpf;
+            }
+            processoService.visualizarProcesso(processo);
+        }
+
+        function concluir() {
+
+            console.log(ctrl.analiseTecnica);
+        }
+
+        function salvar() {
+
+            ctrl.analiseTecnica.analise.processo.empreendimento = null;
+            analiseTecnicaService.salvar(ctrl.analiseTecnica)
+                .then(function (response) {
+
+                    mensagem.success(response.data.texto);
+                    carregarAnalise();
+
+                }, function (error) {
+
+                    mensagem.error(error.data.texto);
+                });
+        }
+
+        function cancelar() {
+
+            $window.history.back();
+        }
+
+        function carregarAnalise() {
+
+            analiseTecnicaService.getAnaliseTecnica(ctrl.analiseTecnica.id)
+                .then(function (response) {
+
+                    ctrl.analiseTecnica = response.data;
+                });
+        }
     }
-
-    function concluir() {
-
-        console.log(ctrl.analiseTecnica);
-    }
-
-    function salvar() {
-
-        analiseTecnicaService.salvar(ctrl.analiseTecnica)
-            .then(function(response) {
-
-                mensagem.success(response.data.texto);
-                carregarAnalise();
-            
-            }, function(error){
-
-                mensagem.error(error.data.texto);
-            });
-    }
-
-    function cancelar() {
-
-        $window.history.back();
-    }
-
-     function carregarAnalise() {
-
-        analiseTecnicaService.getAnaliseTecnica(ctrl.analiseTecnica.id)
-            .then(function(response){
-
-                ctrl.analiseTecnica = response.data;
-            });
-     }    
 };
 
 exports.controllers.AnaliseTecnicaController = AnaliseTecnicaController;
