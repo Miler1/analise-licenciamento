d352fcdf7071288f490cdf0a91b31ca1c6f3e674
Cleiton Luiz <cleiton.teodoro@gt4w.com.br>
2017-06-29 10:42:13 -0300

    Exibiação do resumo do processo na validação da analise tecnica

--- frontend/src/features/aguardandoValidacao/validacaoAnalise/coordenadorTecnico/validacao-analise.controller.js
+++ frontend/src/features/aguardandoValidacao/validacaoAnalise/coordenadorTecnico/validacao-analise.controller.js
@@ -1,11 +1,48 @@
-var ValidacaoAnaliseTecnicaController = function() {
+var ValidacaoAnaliseTecnicaController = function($rootScope, analiseTecnicaService, $route, $scope, 
+		mensagem, $location, documentoAnaliseService, processoService) {
 
     var validacaoAnaliseTecnica = this;
 
-    validacaoAnaliseTecnica.init = function() {
+    validacaoAnaliseTecnica.init = init;
+    validacaoAnaliseTecnica.exibirDadosProcesso = exibirDadosProcesso; 
+    validacaoAnaliseTecnica.exibirAnaliseJuridica = exibirAnaliseJuridica;
 
-        alert('entrou');
-    };
+
+    function init() {
+
+		analiseTecnicaService.getAnaliseTecnica($route.current.params.idAnalise)
+			.then(function(response){
+				validacaoAnaliseTecnica.analiseTecnica = response.data;
+
+			});
+		
+		$rootScope.$broadcast('atualizarContagemProcessos');        
+    }
+
+	function exibirDadosProcesso() {
+
+        var processo = {
+
+            idProcesso: validacaoAnaliseTecnica.analiseTecnica.analise.processo.id,
+            numero: validacaoAnaliseTecnica.analiseTecnica.analise.processo.numero,
+            denominacaoEmpreendimento: validacaoAnaliseTecnica.analiseTecnica.analise.processo.empreendimento.denominacao
+        };
+
+        if(validacaoAnaliseTecnica.analiseTecnica.analise.processo.empreendimento.pessoa.cnpj) {
+
+            processo.cnpjEmpreendimento = validacaoAnaliseTecnica.analiseTecnica.analise.processo.empreendimento.pessoa.cnpj;
+
+        } else {
+
+            processo.cpfEmpreendimento = validacaoAnaliseTecnica.analiseTecnica.analise.processo.empreendimento.pessoa.cpf;
+        }		
+
+        processoService.visualizarProcesso(processo);
+    }    
+
+    function exibirAnaliseJuridica() {
+        
+    }
 
 };
 
