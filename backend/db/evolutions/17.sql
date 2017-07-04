94a7e3abcdc7c10f0bb6c617a21cee786ee6b2c3
Lucas <Lucas>
2017-07-04 11:30:06 -0300

    Adicionando comentários na evolution 18.

--- backend/db/evolutions/17.sql
+++ backend/db/evolutions/17.sql
@@ -12,6 +12,7 @@ JOIN analise.analise a ON a.id_processo=p.id
 WHERE aj.id_analise=a.id AND h.id_condicao_inicial=5 ORDER BY h.id_historico_objeto_tramitavel DESC LIMIT 1
 );
 
+COMMENT ON COLUMN analise.analise_juridica.id_usuario_validacao IS 'Identificador da entidade portal_seguranca.usuario que realizará o relacionamento entre as duas entidades.';
 
 
 ALTER TABLE analise.analise_tecnica ADD COLUMN id_usuario_validacao INTEGER;
@@ -25,6 +26,9 @@ JOIN analise.analise a ON a.id_processo=p.id
 WHERE at.id_analise=a.id AND h.id_condicao_inicial=10 ORDER BY h.id_historico_objeto_tramitavel DESC LIMIT 1
 );
 
+COMMENT ON COLUMN analise.analise_tecnica.id_usuario_validacao IS 'Identificador da entidade portal_seguranca.usuario que realizará o relacionamento entre as duas entidades.';
+
+COMMENT ON COLUMN tramitacao.historico_objeto_tramitavel.id_usuario IS 'Identificador da entidade portal_seguranca.usuario que realizará o relacionamento entre as entidades usuario e histórico_objeto_tramitavel. Identifica qual usuário executou a ação da tramitação.';
 
 
 # --- !Downs
@@ -32,3 +36,4 @@ WHERE at.id_analise=a.id AND h.id_condicao_inicial=10 ORDER BY h.id_historico_ob
 
 ALTER TABLE analise.analise_juridica DROP COLUMN id_usuario_validacao;
 ALTER TABLE analise.analise_tecnica DROP COLUMN id_usuario_validacao;
+COMMENT ON COLUMN tramitacao.historico_objeto_tramitavel.id_usuario IS 'Identificador da entidade usr_geocar_aplicacao.usuario que realizará o relacionamento entre as entidades usuario e histórico_objeto_tramitavel. Identifica qual usuário executou a ação da tramitação.';
