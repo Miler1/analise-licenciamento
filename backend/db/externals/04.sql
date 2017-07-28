b035e5f7d8a73d0f7b75b9dbf8e9d3c82cb886f2
Lucas <Lucas>
2017-07-28 10:37:44 -0300

    Alterando a external 04

--- backend/db/externals/04.sql
+++ backend/db/externals/04.sql
@@ -74,26 +74,6 @@ VALUES (7, (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'VINCULAR_P
 -- Remoção da permissão de visualizar o modulo de analise para o perfil externo (este só deve ter permissões referentes ao modulo de licenciamento)
 DELETE FROM portal_seguranca.permissao_perfil where id_perfil = 2 AND id_permissao = (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'acessarAnaliseLicenciamento');
 
--- Insert dos setores relacionados ao novos perfis
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (1, 'Secretaria Adjunta de Gestão e Regularidade Ambiental', 'SAGRA', NULL, NULL, 0);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (2, 'Diretoria de Gestão Florestal e Agrossilvipastoril', 'DGFLOR', NULL, 9, 1);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (3, 'Diretoria de Licenciamento Ambiental', 'DLA', NULL, 9, 1);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (4, 'Coordenadoria  de Gestão Agropastoril e Industrial', 'COGAPI', 2, 6, 2);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (5, 'Coordenadoria de Gestão Florestal', 'COGEF', 2, 6, 2);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (6, 'Coordenadoria de Indústria Comércio Serviços e Resíduos', 'CIND', 3, 6, 2);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (7, 'Coordenadoria de Infraestrutura, Fauna, Aquicultura e Pesca', 'CINFAP', 3, 6, 2);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (8, 'Coordenadoria de Mineração', 'CMINA', 3, 6, 2);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (9, 'Gerência de Atividades Agropecuárias', 'GEAGRO', 4, 8, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (10, 'Gerência de Cadastro, Transporte e Comercialização de Produtos e Subprodutos Florestais', 'GESFLORA', 5, 7, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (11, 'Gerência de Fauna, Flora, Aquicultura e Pesca', 'GEFAP', 7, 7, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (12, 'Gerência de Infraestrutura de Transporte e Obras Civis', 'GEINFRA', 7, 7, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (13, 'Gerência de Projetos de Comércio e Serviços', 'GECOS', 6, 7, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (14, 'Gerência de Projetos de Processamento de Produtos e Subprodutos Florestais', 'GEPROF', 4, 8, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (15, 'Gerência de Projetos Industriais', 'GEIND', 6, 7, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (16, 'Gerência de Projetos Minerários Metálicos', 'GEMIM', 8, 7, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (17, 'Gerência de Projetos Minerários Não Metálicos', 'GEMINA', 8, 7, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (18, 'Gerência de Projetos Silvipastoris', 'GEPAF', 5, 7, 3);
-INSERT INTO portal_seguranca.setor (id, nome, sigla, id_setor_pai, id_perfil, tipo_setor) VALUES (19, 'Gerência de Infraestrutura de Energia, Parcelamento do Solo e Saneamento', 'GEPAS', 7, 7, 3);
 
 
 # --- !Downs
@@ -105,4 +85,3 @@ DELETE FROM portal_seguranca.permissao_perfil WHERE id_perfil IN (3,4,5,6,7,8,9)
 DELETE FROM portal_seguranca.permissao WHERE codigo='LISTAR_PROCESSO_JURIDICO'; 
 DELETE FROM portal_seguranca.permissao WHERE codigo='VINCULAR_PROCESSO_JURIDICO'; 
 DELETE FROM portal_seguranca.perfil WHERE id IN (3,4,5,6,7,8,9);
-DELETE FROM portal_seguranca.setor WHERE id IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19);
