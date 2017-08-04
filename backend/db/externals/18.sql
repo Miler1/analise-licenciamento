9f7fdd2a40d968a46e646c0f6a3c5ac033aa0fb8
Lucas <Lucas>
2017-08-04 17:31:30 -0300

    Arrumando external 18

--- backend/db/externals/18.sql
+++ backend/db/externals/18.sql
@@ -1,124 +1,125 @@
 # --- !Ups
 
 
-UPDATE tramitacao.condicao SET nm_condicao='Aguardando vinculação técnica pelo gerente' WHERE nm_condicao ILIKE 'Aguardando vinculação técnica';
-UPDATE tramitacao.condicao SET nm_condicao='Aguardando validação técnica pelo gerente' WHERE nm_condicao ILIKE 'Aguardando validação técnica';
+UPDATE tramitacao.condicao SET nm_condicao='Aguardando vinculação técnica pelo gerente' WHERE id_condicao=7;
+UPDATE tramitacao.condicao SET nm_condicao='Aguardando validação técnica pelo gerente' WHERE id_condicao=10;
 
-INSERT INTO tramitacao.condicao(id_etapa,nm_condicao,fl_ativo) VALUES
-(2,'Aguardando vinculação técnica pelo coordenador',1),
-(2,'Aguardando validação técnica pelo coordenador',1);
-
-
-UPDATE tramitacao.acao SET tx_descricao='Deferir análise técnica via gerente' WHERE tx_descricao ILIKE 'Deferir análise técnica';
-UPDATE tramitacao.acao SET tx_descricao='Indeferir análise técnica via gerente' WHERE tx_descricao ILIKE 'Indeferir análise técnica';
-UPDATE tramitacao.acao SET tx_descricao='Validar deferimento técnico pelo gerente' WHERE tx_descricao ILIKE 'Validar deferimento técnico';
-UPDATE tramitacao.acao SET tx_descricao='Invalidar parecer técnico pelo gerente' WHERE tx_descricao ILIKE 'Invalidar parecer técnico';
-UPDATE tramitacao.acao SET tx_descricao='Solicitar ajustes parecer técnico pelo gerente' WHERE tx_descricao ILIKE 'Solicitar ajustes parecer técnico';
-UPDATE tramitacao.acao SET tx_descricao='Validar indeferimento técnico pelo gerente' WHERE tx_descricao ILIKE 'Validar indeferimento técnico';
-
-INSERT INTO tramitacao.acao(tx_descricao,fl_ativo,fl_tramitavel) VALUES
-('Vincular gerente',1,1),
-('Indeferir análise técnica via coordenador',1,1),
-('Deferir análise técnica via coordenador',1,1),
-('Validar deferimento técnico pelo coordenador',1,1),
-('Validar indeferimento técnico pelo coordenador',1,1),
-('Invalidar parecer técnico pelo coordenador encaminhando para outro gerente',1,1),
-('Invalidar parecer técnico encaminhando para outro técnico',1,1),
-('Solicitar ajuste do parecer técnico pelo coordenador',1,1);
+INSERT INTO tramitacao.condicao(id_condicao,id_etapa,nm_condicao,fl_ativo) VALUES
+(12,2,'Aguardando vinculação técnica pelo coordenador',1),
+(13,2,'Aguardando validação técnica pelo coordenador',1);
 
 
-UPDATE tramitacao.transicao SET id_condicao_final=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo coordenador')
-WHERE id_condicao_inicial=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação jurídica')
-AND id_condicao_final=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo gerente') 
-AND id_acao=(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Validar deferimento jurídico');
 
-UPDATE tramitacao.transicao SET id_condicao_final=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador') 
-WHERE id_condicao_inicial=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo gerente') 
-AND id_acao=(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Validar deferimento técnico pelo gerente');
+UPDATE tramitacao.acao SET tx_descricao='Deferir análise técnica via gerente' WHERE id_acao=12;
+UPDATE tramitacao.acao SET tx_descricao='Indeferir análise técnica via gerente' WHERE id_acao=13;
+UPDATE tramitacao.acao SET tx_descricao='Validar deferimento técnico pelo gerente' WHERE id_acao=16;
+UPDATE tramitacao.acao SET tx_descricao='Invalidar parecer técnico pelo gerente' WHERE id_acao=14;
+UPDATE tramitacao.acao SET tx_descricao='Solicitar ajustes parecer técnico pelo gerente' WHERE id_acao=15;
+UPDATE tramitacao.acao SET tx_descricao='Validar indeferimento técnico pelo gerente' WHERE id_acao=17;
 
-UPDATE tramitacao.transicao SET id_condicao_final=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador') 
-WHERE id_condicao_inicial=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo gerente') 
-AND id_acao=(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Invalidar deferimento técnico pelo gerente');
+INSERT INTO tramitacao.acao(id_acao,tx_descricao,fl_ativo,fl_tramitavel) VALUES
+(19,'Vincular gerente',1,1),
+(20,'Indeferir análise técnica via coordenador',1,1),
+(21,'Deferir análise técnica via coordenador',1,1),
+(22,'Validar deferimento técnico pelo coordenador',1,1),
+(23,'Validar indeferimento técnico pelo coordenador',1,1),
+(24,'Invalidar parecer técnico pelo coordenador encaminhando para outro gerente',1,1),
+(25,'Invalidar parecer técnico encaminhando para outro técnico',1,1),
+(26,'Solicitar ajuste do parecer técnico pelo coordenador',1,1),
+(27,'Invalidar deferimento técnico pelo gerente',1,1),
+(28,'Deferir análise técnica via coordenador',1,1);
 
 
-INSERT INTO tramitacao.transicao(id_condicao_inicial,id_condicao_final,id_acao) VALUES
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo coordenador'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo gerente'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Vincular gerente')),
+UPDATE tramitacao.transicao SET id_condicao_final=12 WHERE id_condicao_inicial=5 AND id_condicao_final=7 AND id_acao=6;
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo gerente'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Validar deferimento técnico pelo gerente')),
+UPDATE tramitacao.transicao SET id_condicao_final=13 
+WHERE id_condicao_inicial=10
+AND id_acao=16;
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando assinatura do diretor'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Validar deferimento técnico pelo coordenador')),
+UPDATE tramitacao.transicao SET id_condicao_final=13 
+WHERE id_condicao_inicial=10 
+AND id_acao=27;
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Arquivado'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Validar indeferimento técnico pelo coordenador')),
+UPDATE tramitacao.transicao SET id_condicao_final = 13 WHERE id_transicao = 17;
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando análise técnica'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Invalidar parecer técnico encaminhando para outro técnico')),
+INSERT INTO tramitacao.transicao(id_transicao,id_condicao_inicial,id_condicao_final,id_acao) VALUES
+(20,12,7,19),
+(21,10,11,16),
+(22,13,11,22),
+(23,13,6,23),
+(24,13,8,25),
+(25,13,7,24),
+(26,13,8,26),
+(27,12,9,10),
+(28,9,13,28),
+(29,9,13,20);
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo gerente'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Invalidar parecer técnico pelo coordenador encaminhando para outro gerente')),
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (25, 1, 27, 1);
+--Ação 20 (Indeferir análise técnica via coordenador) - Adicionar situação "Indeferido" - (Nº da transição: 28)
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (26, 2, 28, 1);
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando análise técnica'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Solicitar ajuste do parecer técnico pelo coordenador')),
+--Ação 22 (Validar deferimento técnico pelo coordenador) - Remover situações "Deferido" e "Indeferido" - (Nº da transição: 21)
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (27, 1, 21, 0);
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (28, 2, 21, 0);
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo coordenador'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Em análise técnica'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Vincular analista')),
+--Ação 23 (Validar indeferimento técnico pelo coordenador) - Remover situações "Deferido" e "Indeferido" - (Nº da transição: 22)
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (29, 1, 22, 0);
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (30, 2, 22, 0);
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Em análise técnica'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Deferir análise técnica via coordenador')),
+--Ação 24 (Invalidar parecer técnico pelo coordenador encaminhando para outro gerente) - Remover situações "Deferido" e "Indeferido" - (Nº da transição: 24)
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (31, 1, 24, 0);
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (32, 2, 24, 0);
 
-((SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Em análise técnica'),
-(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador'),
-(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Indeferir análise técnica via coordenador'));
+--Ação 25 (Invalidar parecer técnico encaminhando para outro técnico) - Remover situações "Deferido" e "Indeferido" - (Nº da transição: 23)
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (33, 1, 23, 0);
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (34, 2, 23, 0);
+
+--Ação 26 (Solicitar ajuste do parecer técnico pelo coordenador) - Remover situações "Deferido" e "Indeferido" - (Nº da transição: 25)
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (35, 1, 25, 0);
+INSERT INTO tramitacao.config_situacao (id_config_situacao, id_situacao, id_transicao, fl_adicionar) VALUES (36, 2, 25, 0);
+
+
+-- [FIM] Configurar adição e remoção de situações
+
+-- [INICIO] Configuração dos impedimentos
+
+--Ação 16 (Validar deferimento técnico pelo gerente) - Situação "Deferido" obrigatória - (Nº da transição: 16)
+INSERT INTO tramitacao.impedimento_transicao (id_impedimento_transicao, id_situacao, id_transicao, tp_impedimento) VALUES (3, 1, 16, 1);
+--Ação 17 (Validar indeferimento técnico pelo gerente) - Situação "Indeferido" obrigatória - (Nº da transição: 17)
+INSERT INTO tramitacao.impedimento_transicao (id_impedimento_transicao, id_situacao, id_transicao, tp_impedimento) VALUES (4, 2, 17, 1);
+
+--Ação 22 (Validar deferimento técnico pelo coordenador) - Situação "Deferido" obrigatória - (Nº da transição: 21)
+INSERT INTO tramitacao.impedimento_transicao (id_impedimento_transicao, id_situacao, id_transicao, tp_impedimento) VALUES (5, 1, 21, 1);
+--Ação 23 (Validar indeferimento pelo coordenador) - Situação "Indeferido" obrigatória - (Nº da transição: 22)
+INSERT INTO tramitacao.impedimento_transicao (id_impedimento_transicao, id_situacao, id_transicao, tp_impedimento) VALUES (6, 2, 22, 1);
 
 
 # --- !Downs
 
+DELETE FROM tramitacao.impedimento_transicao WHERE id_impedimento_transicao IN (3,4,5,6);
 
-UPDATE tramitacao.transicao SET id_condicao_final=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo gerente')
-WHERE id_condicao_inicial=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação jurídica')
-AND id_condicao_final=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo coordenador') 
-AND id_acao=(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Validar deferimento jurídico');
+DELETE FROM tramitacao.config_situacao WHERE id_config_situacao IN(25,26,27,28,29,30,31,32,33,34,35);
 
-UPDATE tramitacao.transicao SET id_condicao_final=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo gerente') 
-WHERE id_condicao_inicial=(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo gerente') 
-AND id_acao=(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Validar deferimento técnico pelo gerente');
+UPDATE tramitacao.transicao SET id_condicao_final=7 WHERE id_condicao_inicial=5 AND id_condicao_final=12 AND id_acao=6;
 
-DELETE FROM tramitacao.transicao WHERE id_transicao BETWEEN 
-	(SELECT id_transicao FROM tramitacao.transicao WHERE id_condicao_inicial=(SELECT id_condicao FROM tramitacao.condicao 
-		WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo coordenador') 
-		AND id_condicao_final =(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo gerente')
-		AND id_acao =(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Vincular gerente')
-	)
-   AND
-	(SELECT id_transicao FROM tramitacao.transicao WHERE id_condicao_inicial=(SELECT id_condicao FROM tramitacao.condicao 
-		WHERE nm_condicao ILIKE 'Em análise técnica') 
-		AND id_condicao_final =(SELECT id_condicao FROM tramitacao.condicao WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo coordenador')
-		AND id_acao =(SELECT id_acao FROM tramitacao.acao WHERE tx_descricao ILIKE 'Indeferir análise técnica via coordenador')
-	);
+UPDATE tramitacao.transicao SET id_condicao_final=10
+WHERE id_condicao_inicial=10
+AND id_acao=16;
 
-UPDATE tramitacao.condicao SET nm_condicao='Aguardando vinculação técnica' WHERE nm_condicao ILIKE 'Aguardando vinculação técnica pelo gerente';
-UPDATE tramitacao.condicao SET nm_condicao='Aguardando validação técnica' WHERE nm_condicao ILIKE 'Aguardando validação técnica pelo gerente';
+UPDATE tramitacao.transicao SET id_condicao_final = 6 WHERE id_transicao = 17;
 
-DELETE FROM tramitacao.condicao WHERE nm_condicao IN ('Aguardando vinculação técnica pelo coordenador','Aguardando validação técnica pelo coordenador');
+DELETE FROM tramitacao.transicao WHERE id_transicao IN (20,21,22,23,24,25,26,27,28,29);
 
-UPDATE tramitacao.acao SET tx_descricao='Deferir análise técnica' WHERE tx_descricao ILIKE 'Deferir análise técnica via gerente';
-UPDATE tramitacao.acao SET tx_descricao='Indeferir análise técnica' WHERE tx_descricao ILIKE 'Indeferir análise técnica via gerente';
-UPDATE tramitacao.acao SET tx_descricao='Validar deferimento técnico' WHERE tx_descricao ILIKE 'Validar deferimento técnico pelo gerente';
-UPDATE tramitacao.acao SET tx_descricao='Invalidar parecer técnico' WHERE tx_descricao ILIKE 'Invalidar parecer técnico pelo gerente';
-UPDATE tramitacao.acao SET tx_descricao='Solicitar ajustes parecer técnico' WHERE tx_descricao ILIKE 'Solicitar ajustes parecer técnico pelo gerente';
-UPDATE tramitacao.acao SET tx_descricao='Validar indeferimento técnico' WHERE tx_descricao ILIKE 'Validar indeferimento técnico pelo gerente';
+DELETE FROM tramitacao.acao WHERE id_acao IN (19,20,21,22,23,24,25,26,27,28);
 
-DELETE FROM tramitacao.acao WHERE tx_descricao IN
-('Vincular gerente','Indeferir análise técnica via coordenador','Deferir análise técnica via coordenador','Validar deferimento técnico pelo coordenador','Validar indeferimento técnico pelo coordenador','Invalidar parecer técnico pelo coordenador encaminhando para outro gerente','Invalidar parecer técnico encaminhando para outro técnico','Solicitar ajuste do parecer técnico pelo coordenador');
+UPDATE tramitacao.acao SET tx_descricao='Deferir análise técnica' WHERE id_acao=12;
+UPDATE tramitacao.acao SET tx_descricao='Indeferir análise técnica' WHERE id_acao=13;
+UPDATE tramitacao.acao SET tx_descricao='Validar deferimento técnico' WHERE id_acao=16;
+UPDATE tramitacao.acao SET tx_descricao='Invalidar parecer técnico pelo' WHERE id_acao=14;
+UPDATE tramitacao.acao SET tx_descricao='Solicitar ajustes parecer técnico' WHERE id_acao=15;
+UPDATE tramitacao.acao SET tx_descricao='Validar indeferimento técnico' WHERE id_acao=17;
+
+UPDATE tramitacao.condicao SET nm_condicao='Aguardando vinculação técnica' WHERE id_condicao=7;
+UPDATE tramitacao.condicao SET nm_condicao='Aguardando validação técnica' WHERE id_condicao=10;
+
+DELETE FROM tramitacao.condicao WHERE id_condicao IN (12,13);
