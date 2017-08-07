87943901021cbd9e834ba96d317728a0abd5215d
Matheus <matheus.barbosa@gt4w.com>
2017-08-07 09:26:58 -0300

    Criação da tela de validação do parecer técnico

--- frontend/src/features/aguardandoValidacao/aguardandoValidacao.module.js
+++ frontend/src/features/aguardandoValidacao/aguardandoValidacao.module.js
@@ -26,4 +26,5 @@ aguardandoValidacao
 	.controller('aguardandoValidacaoJuridicoListController', controllers.AguardandoValidacaoJuridicoListController)
 	.controller('validacaoAnaliseJuridicaController', controllers.ValidacaoAnaliseJuridicaController)
 	.controller('aguardandoValidacaoTecnicaListController', controllers.AguardandoValidacaoTecnicaListController)
-	.controller('validacaoAnaliseTecnicaController', controllers.ValidacaoAnaliseTecnicaController);
+	.controller('validacaoAnaliseTecnicaController', controllers.ValidacaoAnaliseTecnicaController)
+	.controller('validacaoAnaliseTecnicaGerenteController', controllers.ValidacaoAnaliseTecnicaGerenteController);
