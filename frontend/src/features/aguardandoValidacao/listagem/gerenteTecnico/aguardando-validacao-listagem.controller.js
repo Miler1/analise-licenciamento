99061381758197bca82919da9b2fe371ac3f512d
Julio Cezar <julio.diniz@lemaf.ufla.br>
2017-08-09 15:24:23 -0300

    Alterado a condição de tramitação enviada para o backend

--- frontend/src/features/aguardandoValidacao/listagem/gerenteTecnico/aguardando-validacao-listagem.controller.js
+++ frontend/src/features/aguardandoValidacao/listagem/gerenteTecnico/aguardando-validacao-listagem.controller.js
@@ -11,7 +11,7 @@ var AguardandoValidacaoGerenteTecnicoListController = function($scope, config, $
 	listagem.validarAnalise = validarAnalise;
 
 	listagem.processos = [];
-	listagem.condicaoTramitacao = app.utils.CondicaoTramitacao.AGUARDANDO_VALIDACAO_TECNICA;
+	listagem.condicaoTramitacao = app.utils.CondicaoTramitacao.AGUARDANDO_VALIDACAO_TECNICA_PELO_GERENTE;
 	listagem.paginacao = new app.utils.Paginacao(config.QTDE_ITENS_POR_PAGINA);
 	listagem.PrazoMinimoAvisoAnalise = app.utils.PrazoMinimoAvisoAnalise;
 	listagem.dateUtil = app.utils.DateUtil;
