174ebd241f5052177ad5f86b27a16d89f696971d
Lucas <Lucas>
2017-08-16 15:20:17 -0300

    Alterando a tabela analise.analise_tecnica adicionando o campo data_cadastro.

---
+++ backend/db/evolutions/23.sql
@@ -0,0 +1,12 @@
+# --- !Ups
+
+ALTER TABLE analise.analise_tecnica ADD COLUMN data_cadastro TIMESTAMP WITHOUT TIME ZONE;
+COMMENT ON COLUMN analise.analise_tecnica.data_cadastro IS 'Data de cadastro da análise.';
+
+UPDATE analise.analise_tecnica SET data_cadastro=(data_vencimento_prazo-10);
+
+ALTER TABLE analise.analise_tecnica ALTER COLUMN data_cadastro SET NOT NULL
+
+# --- !Downs
+
+ALTER TABLE analise.analise_tecnica DROP COLUMN data_cadastro
