7139cd11bd9674899361f932433448727148cd31
gabrielacampos <gabriela.campos@gt4w.com.br>
2017-08-24 09:57:55 -0300

    Aprovador: Tela para exibir informações gerais

---
+++ frontend/src/features/aguardandoAssinatura/validacaoAnalise/aprovador/validacao-analise.controller.js
@@ -0,0 +1,33 @@
+var ValidacaoAnaliseAprovadorController = function($rootScope,aprovadorService, $route, $scope, 
+		mensagem, $location,processoService, $uibModal) {
+
+    var validacaoAnaliseAprovador = this;
+
+    validacaoAnaliseAprovador.exibirDadosProcesso = exibirDadosProcesso;
+
+
+    function exibirDadosProcesso() {
+
+        var processo = {
+
+            idProcesso: validacaoAnaliseAprovador.analiseTecnica.analise.processo.id,
+            numero: validacaoAnaliseAprovador.analiseTecnica.analise.processo.numero,
+            denominacaoEmpreendimento: validacaoAnaliseAprovador.analiseTecnica.analise.processo.empreendimento.denominacao
+        };
+
+        if(validacaoAnaliseAprovador.analiseTecnica.analise.processo.empreendimento.pessoa.cnpj) {
+
+            processo.cnpjEmpreendimento = validacaoAnaliseAprovador.analiseTecnica.analise.processo.empreendimento.pessoa.cnpj;
+
+        } else {
+
+            processo.cpfEmpreendimento = validacaoAnaliseAprovador.analiseTecnica.analise.processo.empreendimento.pessoa.cpf;
+        }		
+
+        processoService.visualizarProcesso(processo);
+    }    
+        
+
+};
+
+exports.controllers.ValidacaoAnaliseAprovadorController = ValidacaoAnaliseAprovadorController;
