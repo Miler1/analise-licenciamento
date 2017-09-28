3ffe37010da97c5072b974d480d3930728454c0c
João S. Silva <joao.silva@gt4w.com.br>
2017-09-28 14:32:24 -0300

    correções na identação do código

--- backend/app/jobs/ProcessamentoProcessosEmAnalise.java
+++ backend/app/jobs/ProcessamentoProcessosEmAnalise.java
@@ -22,69 +22,67 @@ public class ProcessamentoProcessosEmAnalise extends GenericJob {
 
 	@Override
 	public void executar() throws Exception {
-		
+
 		Logger.info("[INICIO-JOB] ::ProcessamentoProcessosEmAnalise:: [INICIO-JOB]");
 		recuperarProcessosEmAnalise();
 		Logger.info("[FIM-JOB] ::ProcessamentoProcessosEmAnalise:: [FIM-JOB]");
-		
-		
+
 	}
 	
 	public void recuperarProcessosEmAnalise() {
-		
+
 		List<DiasAnalise> dAnalise = DiasAnalise.findAll();
-		
+
 		if(dAnalise.size() == 0) {
-			
+
 			List<Analise> analises = Analise.findAll();
-			
+
 			for(Analise analise : analises) {
-				
+
 				DiasAnalise diasAnalise = new DiasAnalise();
-	
+
 				if (analise.getAnaliseTecnica() != null) {
 				// se existe analise tecnica
-					
+
 					if (analise.analiseTecnica.dataFim != null) {
 					// se a analise tecnica acabou
-					
+
 						diasAnalise.qtdeDiasAnalise = CalculaDiferencaDias(analise.dataCadastro, analise.analiseTecnica.dataFim);
 						diasAnalise.qtdeDiasJuridica = CalculaDiferencaDias(analise.dataCadastro, analise.analiseJuridica.dataFim);
 						diasAnalise.qtdeDiasTecnica = CalculaDiferencaDias(analise.analiseTecnica.dataCadastro, analise.analiseTecnica.dataFim);
-					
+
 					} else {
 					// se a analise tecnica nao acabou
-							
+
 						diasAnalise.qtdeDiasAnalise = CalculaDiferencaDias(analise.dataCadastro, new Date());
 						diasAnalise.qtdeDiasJuridica = CalculaDiferencaDias(analise.dataCadastro, analise.analiseJuridica.dataFim);							
 						diasAnalise.qtdeDiasTecnica= CalculaDiferencaDias(analise.analiseTecnica.dataCadastro, new Date());
-							
+
 					}
-					
-					
+
 				} else {
 				// se nao existe analise tecnica 
-					
-					
+
 					if(analise.getAnaliseJuridica() != null) {
 					// se existe analise juridica
 						
 						if(analise.analiseJuridica.dataFim != null) {
 						// se a analise juridica acabou
-							
+
 							diasAnalise.qtdeDiasJuridica = CalculaDiferencaDias(analise.dataCadastro, analise.analiseJuridica.dataFim);
 							
 	
 						} else {
 						// se a analise juridica nao acabou
-						
+
 							diasAnalise.qtdeDiasJuridica = CalculaDiferencaDias(analise.dataCadastro, new Date());
-		
+
 						}
+
 						diasAnalise.qtdeDiasAnalise = diasAnalise.qtdeDiasJuridica;
 				
 					}
-					
+
 				}
 
 				diasAnalise.analise = analise;
