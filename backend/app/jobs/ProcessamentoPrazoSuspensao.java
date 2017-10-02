9c1c90e951e1f2acfa1d4ced595fae2afbea5918
gabrielacampos <gabriela.campos@gt4w.com.br>
2017-10-02 18:04:15 -0300

    Iniciando job para verificar prazo de suspensao

---
+++ backend/app/jobs/ProcessamentoPrazoSuspensao.java
@@ -0,0 +1,41 @@
+package jobs;
+
+import java.util.List;
+
+import models.Suspensao;
+import play.Logger;
+import play.jobs.On;
+
+@On("0 0/1 * 1/1 * ? *")
+public class ProcessamentoPrazoSuspensao  extends GenericJob{
+	
+	@Override
+	public void executar() throws Exception{
+
+		Logger.info("[INICIO-JOB] ::ProcessamentoPrazoSuspensao:: [INICIO-JOB]");
+		verificaPrazoSuspensao();
+		Logger.info("[FIM-JOB] ::ProcessamentoPrazoSuspensao:: [FIM-JOB]");
+		
+
+	}
+	
+	public void verificaPrazoSuspensao() {
+		
+		List<Suspensao> suspensoes = Suspensao.findAll();
+		
+		if(!suspensoes.isEmpty()) {
+			
+			for(Suspensao suspensao: suspensoes){
+				
+				if(suspensao.dataSuspensao != null) {
+					
+					
+					
+				}
+				
+			}
+		}
+		
+	}
+
+}
