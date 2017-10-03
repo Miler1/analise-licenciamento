1b33a7ca62e2322af97ed0dc5dceb57fbddd67e5
gabrielacampos <gabriela.campos@gt4w.com.br>
2017-10-03 15:51:09 -0300

    Verificando se a data é igual a de hoje

--- backend/app/jobs/ProcessamentoPrazoSuspensao.java
+++ backend/app/jobs/ProcessamentoPrazoSuspensao.java
@@ -1,19 +1,24 @@
 package jobs;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
 import models.Suspensao;
 import play.Logger;
 import play.jobs.On;
 
 @On("0 0/1 * 1/1 * ? *")
-public class ProcessamentoPrazoSuspensao  extends GenericJob{
+public class ProcessamentoPrazoSuspensao  extends GenericJob {
 	
 	@Override
 	public void executar() throws Exception{
 
 		Logger.info("[INICIO-JOB] ::ProcessamentoPrazoSuspensao:: [INICIO-JOB]");
-		//verificaPrazoSuspensao();
+		verificaPrazoSuspensao();
 		Logger.info("[FIM-JOB] ::ProcessamentoPrazoSuspensao:: [FIM-JOB]");
 		
 
@@ -27,7 +32,13 @@ public class ProcessamentoPrazoSuspensao  extends GenericJob{
 			
 			for(Suspensao suspensao: suspensoes){
 				
-				if(suspensao.dataSuspensao != null) {
+				//mostra a data final da suspensao
+				
+				Date dataFinalSuspenso = addDays(suspensao.dataSuspensao, suspensao.qtdeDiasSuspensao);
+				System.out.println("SOMANDO NA DATA "+suspensao.qtdeDiasSuspensao+" days: "+dataFinalSuspenso.toString());
+				Date hoje =  new Date();
+				
+				if(dataFinalSuspenso ==  hoje) {
 					
 					
 					
@@ -36,6 +47,15 @@ public class ProcessamentoPrazoSuspensao  extends GenericJob{
 			}
 		}
 		
+	}
+	
+	public static Date addDays(Date date, int days) {
+		
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.setTime(date);
+		cal.add(Calendar.DATE, days);
+				
+		return cal.getTime();
 	}
 
 }
--- backend/app/jobs/ProcessamentoPrazos.java
+++ backend/app/jobs/ProcessamentoPrazos.java
@@ -20,7 +20,7 @@ import play.Logger;
 import play.jobs.On;
 import utils.Configuracoes;
 
-@On("cron.processamentoPrazos")
+@On("0 0/1 * 1/1 * ? *")
 public class ProcessamentoPrazos extends GenericJob {
 
 	@Override
