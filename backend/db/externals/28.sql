6e88f0ba13ed609d4c41784a3cc94521de90d3a1
Renan Oliveira <renanoliveira.lemaf@gmail.com>
2017-10-16 09:11:20 -0200

    Atualização da external 28. Correção.

--- backend/db/externals/28.sql
+++ backend/db/externals/28.sql
@@ -1,11 +1,13 @@
 # --- !Ups
 
-ALTER TABLE analise.suspensao ADD CONSTRAINT fk_usuario_suspensao FOREIGN KEY(id_usuario_suspensao) 
+ALTER TABLE analise.licenca_suspensa ADD CONSTRAINT fk_usuario_suspensao FOREIGN KEY(id_usuario_executor) 
  REFERENCES portal_seguranca.usuario(id);
  
- ALTER TABLE analise.suspensao ADD CONSTRAINT fk_licenca_suspensao FOREIGN KEY(id_licenca) 
+ ALTER TABLE analise.licenca_suspensa ADD CONSTRAINT fk_licenca_suspensao FOREIGN KEY(id_licenca) 
  REFERENCES licenciamento.licenca(id);
  
  # --- !Downs
 
-ALTER TABLE analise.suspensao DROP CONSTRAINT fk_usuario_suspensao;
+ALTER TABLE analise.licenca_suspensa DROP CONSTRAINT fk_usuario_suspensao;
+
+ALTER TABLE analise.licenca_suspensa DROP CONSTRAINT fk_licenca_suspensao;
