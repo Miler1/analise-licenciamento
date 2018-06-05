4748d91b5fd4470dbeca6d6330e2a9d5c5512694
bruno.carvalho <bruno.carvalho@gt4w.com.br>
2018-06-05 15:23:38 -0300

    [WIP] - adicionando controler de qrcode e views de visualização do mesmo

--- backend/app/controllers/Notificacoes.java
+++ backend/app/controllers/Notificacoes.java
@@ -3,6 +3,12 @@ package controllers;
 import models.AnaliseJuridica;
 import models.Documento;
 import models.Notificacao;
+import org.apache.commons.collections.FastHashMap;
+import play.libs.Crypto;
+import utils.Configuracoes;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 public class Notificacoes extends InternalController {
 
@@ -19,6 +25,23 @@ public class Notificacoes extends InternalController {
         response.setHeader("Content-Type", "application/pdf");
 
         renderBinary(pdfNotificacao.arquivo, nome);
+
+    }
+
+
+    public static void qrcodeView(String idNotificacao) throws UnsupportedEncodingException {
+
+        Notificacao notificacao = Notificacao.findById(Crypto.decryptAES(idNotificacao));
+
+        String url = Configuracoes.APP_URL + "/licenca/" + Crypto.encryptAES(notificacao.id.toString()) + "/download";
+        String nomeArquivo = notificacao.tipoDocumento + "_" + notificacao.id + ".pdf";
+
+        Map<String, Object> args = new FastHashMap(3);
+        args.put("notificacao", notificacao);
+        args.put("urlDownload", url);
+        args.put("nomeArquivo", nomeArquivo);
+
+        renderTemplate(Configuracoes.PDF_TEMPLATES_FOLDER_PATH + "/qrcode/informacoes.html", args);
 
     }
 }
