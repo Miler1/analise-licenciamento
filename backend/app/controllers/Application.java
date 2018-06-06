11b37fab523b001af762c715bc2d3ad460d454b2
Enrico Navarro <enrico.navarro@gt4w.com.br>
2018-06-06 12:05:52 -0300

    Correção da função de qrCodeView. Correção da rota de validar um qrCode.

--- backend/app/controllers/Application.java
+++ backend/app/controllers/Application.java
@@ -10,21 +10,22 @@ import serializers.ApplicationSerializer;
 import utils.Configuracoes;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.Map;
 
 public class Application extends GenericController {
 
-    public static void index() {
-        
+	public static void index() {
+
 		UsuarioSessao usuarioSessao = Auth.getUsuarioSessao(session.current());
 		
 		if (usuarioSessao != null)
 			redirect(Configuracoes.INDEX_URL);
 		else
 			redirect(Configuracoes.LOGIN_URL);
-    	
-    }
-    
+
+	}
+
 	public static void findInfo() {
 		
 		DadosApp dados = new DadosApp();
@@ -55,17 +56,9 @@ public class Application extends GenericController {
 	}
 
 
-	public static void qrCodeView(String idNotificacao) throws UnsupportedEncodingException {
+	public static void qrCodeView(String idQrCode) throws UnsupportedEncodingException {
 
-		Notificacao notificacao = Notificacao.findById(Crypto.decryptAES(idNotificacao));
-
-		String url = Configuracoes.APP_URL + "notificacoes/" + idNotificacao + "/download";
-		String nomeArquivo = notificacao.tipoDocumento + "_" + notificacao.id + ".pdf";
-
-		Map<String, Object> args = new FastHashMap(3);
-		args.put("notificacao", notificacao);
-		args.put("urlDownload", url);
-		args.put("nomeArquivo", nomeArquivo);
+		Map<String, Object> args = generateArgs(idQrCode);
 
 		renderTemplate(Configuracoes.PDF_TEMPLATES_FOLDER_PATH + "/qrcode/informacoes.html", args);
 	}
@@ -73,5 +66,37 @@ public class Application extends GenericController {
 	public static void qrCodeDownload(String idNotificacao) throws Exception {
 
 		Notificacoes.downloadPDF(Integer.parseInt(Crypto.decryptAES(idNotificacao)));
+	}
+
+	private static Map<String, Object> generateArgs(String idQrCode) {
+
+		String[] params = idQrCode.split("/");
+
+		List<Notificacao> notificacoes = Notificacao.find("codigoSequencia = :x AND codigoAno = :y")
+				.setParameter("x", params[0])
+				.setParameter("y", params[1])
+				.fetch();
+
+		Notificacao notificacao = notificacoes.get(0);
+		String url = Configuracoes.APP_URL + "notificacoes/" + notificacao.historicoTramitacao.idHistorico + "/download";
+		String nomeArquivo = notificacao.tipoDocumento + "_" + notificacao.id + ".pdf";
+
+		Map<String, Object> args = new FastHashMap(7);
+		args.put("notificacoes", notificacoes);
+		args.put("urlDownload", url);
+		args.put("nomeArquivo", nomeArquivo);
+
+		if (notificacao.analiseJuridica != null) {
+
+			args.put("analiseEspecifica", notificacao.analiseJuridica);
+			args.put("analiseArea", "ANALISE_JURIDICA");
+
+		} else {
+
+			args.put("analiseEspecifica", notificacao.analiseTecnica);
+			args.put("analiseArea", "ANALISE_TECNICA");
+		}
+
+		return args;
 	}
 }
