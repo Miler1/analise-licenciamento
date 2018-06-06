8f5015b32cddeedb5d1c8a5206d3c2cfe9bb086c
Enrico Navarro <enrico.navarro@gt4w.com.br>
2018-06-06 17:58:24 -0300

    Alteração no application.conf para remoção de variável desnecessária. Alteração da função de generateArgs para criptografar a url de download.

--- backend/app/controllers/Application.java
+++ backend/app/controllers/Application.java
@@ -81,7 +81,8 @@ public class Application extends GenericController {
 				.fetch();
 
 		Notificacao notificacao = notificacoes.get(0);
-		String url = Configuracoes.APP_URL + "notificacoes/" + notificacao.historicoTramitacao.idHistorico + "/download";
+		String url = Configuracoes.APP_URL + "notificacoes/" + Crypto.encryptAES(String.valueOf(notificacao.historicoTramitacao.idHistorico))
+				+ "/download";
 
 		Map<String, Object> args = new HashMap<>(7);
 		args.put("notificacoes", notificacoes);
