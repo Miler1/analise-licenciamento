6e434f6b18e8557939f4ff3d58531417b7472881
Gustavo Araujo <gustavo.araujo@gt4w.com.br>
2018-06-12 15:30:32 -0300

    Alteração do template de email de arquivamento.

--- backend/app/utils/tags/TagPDF.java
+++ backend/app/utils/tags/TagPDF.java
@@ -5,7 +5,11 @@ import play.templates.FastTags;
 import play.templates.GroovyTemplate;
 import play.templates.JavaExtensions;
 
+import javax.xml.crypto.Data;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Map;
 
@@ -18,5 +22,22 @@ public class TagPDF extends FastTags {
 		cpf = cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9) + "-" + cpf.substring(9);
 
 		out.println(cpf);
+	}
+
+	public static void _dataFormatada(Map<?, ?> args, Closure body, PrintWriter out,
+							GroovyTemplate.ExecutableTemplate template, int fromLine) {
+
+		String dataString = JavaExtensions.toString(body);
+
+		Date data = null;
+		try {
+
+			data = new SimpleDateFormat("EEE MMM d HH:mm:ss z y").parse(dataString);
+			out.println(new java.text.SimpleDateFormat("dd/MM/yyyy").format(data));
+
+		} catch (ParseException e) {
+
+			e.printStackTrace();
+		}
 	}
 }
