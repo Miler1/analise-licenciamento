b5c75129208d62c5f4d5d85556305a7f172639df
Tiago Paraizo <tiago.gt4w@gmail.com>
2018-06-19 18:05:09 -0300

    Padronização SQL

--- backend/db/utils/script_notificacao_aberta.sql
+++ backend/db/utils/script_notificacao_aberta.sql
@@ -1,4 +1,4 @@
 
 --- Os valores de notificacao_aberta deve ser false quando estiverem NULL
 
-UPDATE analise.analise SET notificacao_aberta=FALSE where notificacao_aberta IS NULL
+UPDATE analise.analise SET notificacao_aberta=FALSE WHERE notificacao_aberta IS NULL;
