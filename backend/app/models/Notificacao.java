1b0ca204cbe70ca4d6efc6e49c50bc1231ac989c
Enrico Navarro <enrico.navarro@gt4w.com.br>
2018-07-18 17:48:57 -0300

    Alteração nos serviços de criar notificação para mudarem o status de uma caracterização para notificado.

--- backend/app/models/Notificacao.java
+++ backend/app/models/Notificacao.java
@@ -18,7 +18,9 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import models.licenciamento.Caracterizacao;
 import models.licenciamento.DocumentoLicenciamento;
+import models.licenciamento.StatusCaracterizacao;
 import models.licenciamento.TipoDocumentoLicenciamento;
 import models.pdf.PDFGenerator;
 import models.tramitacao.HistoricoTramitacao;
@@ -117,6 +119,10 @@ public class Notificacao extends GenericModel {
 		verificaDiasAnalise.qtdeDiasNotificacao = 0;
 		verificaDiasAnalise.save();
 
+		Caracterizacao caracterizacao = analise.processo.caracterizacoes.get(0);
+		caracterizacao.status = StatusCaracterizacao.findById(StatusCaracterizacao.NOTIFICADO);
+		caracterizacao._save();
+
 		analise.temNotificacaoAberta = true;
 		analise._save();
 		
@@ -154,6 +160,10 @@ public class Notificacao extends GenericModel {
 		DiasAnalise verificaDiasAnalise = DiasAnalise.find("analise.id", analise.id).first();
 		verificaDiasAnalise.qtdeDiasNotificacao = 0;
 		verificaDiasAnalise.save();
+
+		Caracterizacao caracterizacao = analise.processo.caracterizacoes.get(0);
+		caracterizacao.status = StatusCaracterizacao.findById(StatusCaracterizacao.NOTIFICADO);
+		caracterizacao._save();
 
 		analise.temNotificacaoAberta = true;
 		analise._save();
