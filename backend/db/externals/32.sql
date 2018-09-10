f4fea7cec20921d73510814a2fddb596be595de0
Gustavo Araujo <gustavo.araujo@gt4w.com.br>
2018-09-10 10:27:05 -0300

    Criação do external para adicionar as ações de renovação de licença para as tramitações

---
+++ backend/db/externals/32.sql
@@ -0,0 +1,17 @@
+# --- !Ups
+
+-- Adicionar ações para a renovação de licencas simples
+
+insert into tramitacao.acao(id_acao, tx_descricao, fl_ativo, fl_tramitavel) VALUES (38, 'Renovar licença sem alterações', 1, 1);
+insert into tramitacao.acao(id_acao, tx_descricao, fl_ativo, fl_tramitavel) VALUES (39, 'Renovar licença com alterações', 1, 1);
+
+insert into tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final, dt_prazo, fl_retornar_fluxo_anterior) VALUES (38, 14, 12, NULL, NULL);
+insert into tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final, dt_prazo, fl_retornar_fluxo_anterior) VALUES (39, 14, 1, NULL, NULL);
+
+# --- !Downs
+
+-- Remover ações que eram para renovação de licencas simples
+
+DELETE FROM tramitacao.transicao WHERE id_acao IN (38, 39);
+DELETE FROM tramitacao.acao WHERE id_acao IN (38, 39);
+
