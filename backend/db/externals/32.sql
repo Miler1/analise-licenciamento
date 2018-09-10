97f63293268feacdaedfa3a2730ed64edfc087cc
Gustavo Araujo <gustavo.araujo@gt4w.com.br>
2018-09-10 10:43:10 -0300

    Padronização codigo external.

--- backend/db/externals/32.sql
+++ backend/db/externals/32.sql
@@ -2,11 +2,11 @@
 
 -- Adicionar ações para a renovação de licencas simples
 
-insert into tramitacao.acao(id_acao, tx_descricao, fl_ativo, fl_tramitavel) VALUES (38, 'Renovar licença sem alterações', 1, 1);
-insert into tramitacao.acao(id_acao, tx_descricao, fl_ativo, fl_tramitavel) VALUES (39, 'Renovar licença com alterações', 1, 1);
+INSERT INTO tramitacao.acao(id_acao, tx_descricao, fl_ativo, fl_tramitavel) VALUES (38, 'Renovar licença sem alterações', 1, 1);
+INSERT INTO tramitacao.acao(id_acao, tx_descricao, fl_ativo, fl_tramitavel) VALUES (39, 'Renovar licença com alterações', 1, 1);
 
-insert into tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final, dt_prazo, fl_retornar_fluxo_anterior) VALUES (38, 14, 12, NULL, NULL);
-insert into tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final, dt_prazo, fl_retornar_fluxo_anterior) VALUES (39, 14, 1, NULL, NULL);
+INSERT INTO tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final, dt_prazo, fl_retornar_fluxo_anterior) VALUES (38, 14, 12, NULL, NULL);
+INSERT INTO tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final, dt_prazo, fl_retornar_fluxo_anterior) VALUES (39, 14, 1, NULL, NULL);
 
 # --- !Downs
 
@@ -14,4 +14,3 @@ insert into tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final
 
 DELETE FROM tramitacao.transicao WHERE id_acao IN (38, 39);
 DELETE FROM tramitacao.acao WHERE id_acao IN (38, 39);
-
