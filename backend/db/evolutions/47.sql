ffbd4407430a8e3339c01c2b95324d1742c42494
Enrico Navarro <enrico.navarro@gt4w.com.br>
2018-09-17 16:31:24 -0300

    Alteração da modelagem e mapeamento de processo para dar suporte ao processo anterior.

--- backend/db/evolutions/47.sql
+++ backend/db/evolutions/47.sql
@@ -4,8 +4,16 @@
 
 ALTER TABLE analise.rel_processo_caracterizacao DROP CONSTRAINT ue_pc_id_caracterizacao;
 
+-- alteração da tabela de processo para incluir o seu processo antigo
+
+ALTER TABLE analise.processo ADD COLUMN id_processo_anterior INTEGER;
+ALTER TABLE analise.processo ADD CONSTRAINT fk_processo_anterior FOREIGN KEY(id_processo_anterior) REFERENCES analise.processo (id);
+
+COMMENT ON COLUMN analise.processo.id_processo_anterior IS 'Identificador da entidade processo que faz o relacionamento entre o processo e o processo anterior a renovação.';
 
 # --- !Downs
+
+ALTER TABLE analise.processo DROP COLUMN id_processo_anterior;
 
 -- voltar restrição de que o campo id_caracterização seja único
 -- TODO precisa de um script de limpeza
