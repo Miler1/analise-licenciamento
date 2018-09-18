4db408420928a409617297b4ae1f5b50b233ec11
Enrico Navarro <enrico.navarro@gt4w.com.br>
2018-09-18 16:57:20 -0300

    Alteração na external 32 para incluir alteração da sequencia de ações em tramitação.

--- backend/db/externals/32.sql
+++ backend/db/externals/32.sql
@@ -8,6 +8,8 @@ INSERT INTO tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final
 INSERT INTO tramitacao.acao(id_acao, tx_descricao, fl_ativo, fl_tramitavel) VALUES (39, 'Renovar licença sem alterações', 1, 1);
 INSERT INTO tramitacao.transicao(id_acao, id_condicao_inicial, id_condicao_final, dt_prazo, fl_retornar_fluxo_anterior) VALUES (39, 0, 12, NULL, NULL);
 
+SELECT pg_catalog.setval('tramitacao.acao_id_acao_seq', 39, TRUE);
+
 # --- !Downs
 
 -- Remover ações que eram para renovação de licencas simples
