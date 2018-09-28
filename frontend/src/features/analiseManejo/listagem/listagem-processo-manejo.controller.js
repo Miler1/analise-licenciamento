1bbe96e82be652e1fdb3b3eb7cd4b7d79577b70e
Gustavo Araujo <gustavo.araujo@gt4w.com.br>
2018-09-28 12:08:26 -0300

    [WIP]- Teste de dados fakes na listagem de processo do manejo

--- frontend/src/features/analiseManejo/listagem/listagem-processo-manejo.controller.js
+++ frontend/src/features/analiseManejo/listagem/listagem-processo-manejo.controller.js
@@ -7,6 +7,8 @@ var ListagemProcessoManejoController = function($scope, config, $rootScope) {
 	listagemProcessoManejo.paginacao = new app.utils.Paginacao(config.QTDE_ITENS_POR_PAGINA);
 	listagemProcessoManejo.atualizarPaginacao = atualizarPaginacao;
 	listagemProcessoManejo.atualizarListaProcessos = atualizarListaProcessos;
+	listagemProcessoManejo.iniciarAnalise = null;
+	listagemProcessoManejo.visualizarProcesso = null;
 	listagemProcessoManejo.processosManejo = [];
 
 	function atualizarPaginacao(totalItens, paginaAtual) {
@@ -18,6 +20,29 @@ var ListagemProcessoManejoController = function($scope, config, $rootScope) {
 
 		listagemProcessoManejo.processosManejo = processos;
 	}
+
+	this.$postLink = function(){
+
+		listagemProcessoManejo.processosManejo = [
+
+			{
+				processo: "1",
+				cpfCnpj: "1",
+				empreendimento: "1",
+				municipioEmpreendimento: "1",
+				siglaEstadoEmpreendimento: "1",
+				licencas: "1",
+			},
+			{
+				processo: "2",
+				cpfCnpj: "2",
+				empreendimento: "2",
+				municipioEmpreendimento: "2",
+				siglaEstadoEmpreendimento: "2",
+				licencas: "2",
+			}
+		];
+	};
 
 };
 
