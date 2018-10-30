8a25ce0c1a698f020033fadc8ed446b2d5ebe2d1
Enrico Navarro <enrico.navarro@gt4w.com.br>
2018-10-30 09:44:41 -0300

    Adição de biblioteca de integração com o entrada única.

--- backend/app/utils/DateUtil.java
+++ backend/app/utils/DateUtil.java
@@ -6,8 +6,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DateUtil {
 
