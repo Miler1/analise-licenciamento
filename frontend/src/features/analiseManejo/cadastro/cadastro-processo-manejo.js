cefc7b1f56059f3b719b59c50ecf76c3ae946085
bruno.carvalho <bruno.carvalho@gt4w.com.br>
2018-11-06 17:14:45 -0200

    feita tela de cadastro manejo

--- frontend/src/features/analiseManejo/cadastro/cadastro-processo-manejo.js
+++ frontend/src/features/analiseManejo/cadastro/cadastro-processo-manejo.js
@@ -2,7 +2,19 @@ var CadastroProcessoManejoController = function($scope, config, $rootScope, proc
 
 	$rootScope.tituloPagina = 'CADASTRAR PROCESSO MANEJO DIGITAL';
 
-	var CadastroProcessoManejoController = this;
+	var cadastroProcessoManejoController = this;
+
+	cadastroProcessoManejoController.processo = null;
+	cadastroProcessoManejoController.tipologia = null;
+	cadastroProcessoManejoController.atividade = null;
+	cadastroProcessoManejoController.municipio = null;
+	cadastroProcessoManejoController.licenca = null;
+	cadastroProcessoManejoController.car = null;
+	cadastroProcessoManejoController.car.numero = null;
+	cadastroProcessoManejoController.car.proprietario.cpfCnpj = null;
+	cadastroProcessoManejoController.car.proprietario.nome = null;
+	cadastroProcessoManejoController.car.proprietario.endereco = null;
+	cadastroProcessoManejoController.car.proprietario.municipio = null;
 };
 
 exports.controllers.CadastroProcessoManejoController = CadastroProcessoManejoController;
