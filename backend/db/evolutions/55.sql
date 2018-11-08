24b08d1b6e512799670292154298e9b2fbea0e93
Enrico Navarro <enrico.navarro@gt4w.com.br>
2018-11-08 14:45:43 -0200

    Criação da evolution 55.

---
+++ backend/db/evolutions/55.sql
@@ -0,0 +1,24 @@
+# --- !Ups
+
+--- Alteração na entidade analise_manejo
+
+ALTER TABLE analise.analise_manejo DROP COLUMN path_arquivo_shape;
+
+ALTER TABLE analise.analise_manejo ADD COLUMN the_geom public.geometry;
+ALTER TABLE analise.analise_manejo ADD CONSTRAINT enforce_dims_geo_localizacao CHECK ((public.st_ndims(the_geom) = 2));
+ALTER TABLE analise.analise_manejo ADD CONSTRAINT enforce_geotype_geo_localizacao CHECK (((public.geometrytype(the_geom) = 'MULTIPOLYGON'::text) OR (the_geom IS NULL)));
+ALTER TABLE analise.analise_manejo ADD CONSTRAINT enforce_srid_geo_localizacao CHECK ((public.st_srid(the_geom) = 4674));
+COMMENT ON COLUMN analise.analise_manejo.the_geom IS 'Coluna georreferenciada.';
+
+ALTER TABLE analise.analise_manejo ADD COLUMN object_id INTEGER;
+COMMENT ON COLUMN analise.analise_manejo.object_id IS 'Identificador da análise no serviço de validação de shape.';
+
+# --- !Downs
+
+ALTER TABLE analise.analise_manejo DROP COLUMN object_id;
+
+ALTER TABLE analise.analise_manejo DROP COLUMN the_geom;
+
+ALTER TABLE analise.analise_manejo ADD COLUMN path_arquivo_shape VARCHAR(500) NOT NULL DEFAULT '';
+ALTER TABLE analise.analise_manejo ALTER COLUMN path_arquivo_shape DROP DEFAULT;
+COMMENT ON COLUMN analise.analise_manejo.path_arquivo_shape IS 'Caminho onde está armazenado o arquivo shape da análise.';
