b04a31dad4a6c4da47b4ed690e3b3ab9a0219e77
Gustavo Araujo <gustavo.araujo@gt4w.com.br>
2018-11-22 10:59:05 -0200

    [WIP] - Upload do shape

--- frontend/src/services/documentoShape.service.js
+++ frontend/src/services/documentoShape.service.js
@@ -10,6 +10,11 @@ var DocumentoShapeService = function(request, config, Upload) {
         return request
             .delete(config.BASE_URL() + "delete/shape/" + nameFile);
     };
+
+	this.download = function(nameFile) {
+
+		return config.BASE_URL() + "download/shape/" + nameFile;
+	};
 };
 
 exports.services.DocumentoShapeService = DocumentoShapeService;
