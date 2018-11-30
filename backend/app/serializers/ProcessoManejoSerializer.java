926fcc4b47241aae4c1b7baf9622f25e74cc00e6
Enrico Navarro <enrico.navarro@gt4w.com.br>
2018-11-30 10:20:45 -0200

    Alteração do serializer para trazer a justificativa de indeferimento.

--- backend/app/serializers/ProcessoManejoSerializer.java
+++ backend/app/serializers/ProcessoManejoSerializer.java
@@ -35,7 +35,8 @@ public class ProcessoManejoSerializer {
 			"empreendimento.imovel.nome",
 			"empreendimento.imovel.municipio.nome",
 			"empreendimento.imovel.municipio.estado.nome",
-			"empreendimento.imovel.descricaoAcesso"
+			"empreendimento.imovel.descricaoAcesso",
+			"justificativaIndeferimento"
 	);
 
 	public static JSONSerializer iniciarAnalise = SerializerUtil.create(
