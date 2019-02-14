cd2c5cd82e5a8f485541e820ee820daa70e91d5f
Enrico Navarro <enrico.navarro@gt4w.com.br>
2019-02-14 12:19:56 -0200

    Remoção de script de banco desnecessário.

--- backend/db/utils/script_integracao_credenciais.sql
+++
@@ -1,3 +0,0 @@
-UPDATE portal_seguranca.cliente_oauth SET id_cliente = 'eb6f4f10a403c38b3225eca778fba0f528c40eaba0ba9d8be967e6533dfa7ab69c1ee2cbb059c634c8c0c735f0714de6b53072493b3328e5d02781470351e2ea',
- segredo = 'ed083340bf2bab15d6da92856a03d80f150cb3460969e337f63f6fc162dee75f83636a28adaad9b3092235b21d55ec9e978cde5de5b51373a53f5368308c42bd' WHERE id = (SELECT id_cliente_oauth
- FROM portal_seguranca.modulo WHERE sigla = 'ALA');
