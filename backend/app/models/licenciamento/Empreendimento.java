ea382e465265c02e676ab634ee75487829136826
Franciele Aparecida Ferreira <franciele.lemaf@gmail.com>
2019-05-10 09:36:27 -0300

    Ajuste erro devido merge com ultimas alteracoes da gt4w

--- backend/app/models/licenciamento/Empreendimento.java
+++ backend/app/models/licenciamento/Empreendimento.java
@@ -4,22 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.*;
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import org.hibernate.annotations.*;
 
@@ -64,12 +52,13 @@ public class Empreendimento extends GenericModel {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "id_contato", referencedColumnName = "id")
 	public Contato contato;
-	
+
 	@Valid
-	@Required
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "id_endereco", referencedColumnName = "id")
-	public Endereco endereco;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(schema = "licenciamento", name = "endereco_empreendimento",
+			joinColumns = @JoinColumn(name = "id_empreendimento"),
+			inverseJoinColumns = @JoinColumn(name = "id_endereco"))
+	public List<Endereco> enderecos;
 	
 	@Column(name="tipo_localizacao")
 	@Enumerated(EnumType.ORDINAL)
