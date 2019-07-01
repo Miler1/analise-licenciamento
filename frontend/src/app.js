9cc673257150fe06e77e55f92face1f17dd17502
Franciele Aparecida Ferreira <franciele.lemaf@gmail.com>
2019-07-01 08:48:18 -0300

    Ajuste erro compilacao logica OU

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -32,8 +32,8 @@ licenciamento.config(["$routeProvider", function($routeProvider) {
 
 					return "/aguardando-assinatura";
 
-				} else if (LICENCIAMENTO_CONFIG.usuarioSessao.usuarioEntradaUnica.perfilSelecionado.codigo === app.utils.Perfis.APROVADOR
-					|| LICENCIAMENTO_CONFIG.usuarioSessao.usuarioEntradaUnica.perfilSelecionado.codigo === app.utils.Perfis.ADMINISTRATIVO_JURIDICO) {
+				} else if (LICENCIAMENTO_CONFIG.usuarioSessao.usuarioEntradaUnica.perfilSelecionado.codigo === app.utils.Perfis.APROVADOR ||
+					LICENCIAMENTO_CONFIG.usuarioSessao.usuarioEntradaUnica.perfilSelecionado.codigo === app.utils.Perfis.ADMINISTRATIVO_JURIDICO) {
 
 					return "/consultar-processo";
 
