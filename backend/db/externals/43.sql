4b77ec9e4eb3d4064a85ad603e7bccbd1065669a
Gabriel Oliveira <gabriel.oliveira.lemaf@gmail.com>
2019-07-12 15:32:33 -0300

    Criando entidades para armazenamento dos shapes de anexo do empreendimento e da entidade pessoa para uso da tramitação, garantindo grants de permissão para a tramitação

---
+++ backend/db/externals/43.sql
@@ -0,0 +1,32 @@
+# --- !Ups
+
+BEGIN;
+
+ALTER TABLE licenciamento.empreendimento ADD COLUMN possui_anexo BOOLEAN;
+
+UPDATE licenciamento.empreendimento SET possui_anexo = false WHERE possui_anexo is null;
+
+ALTER TABLE licenciamento.empreendimento ALTER COLUMN possui_anexo SET NOT NULL;
+ALTER TABLE licenciamento.empreendimento ALTER COLUMN possui_anexo SET DEFAULT FALSE;
+
+ALTER TABLE analise.analise_geo_anexo
+    ADD CONSTRAINT fk_aga_empreendimento FOREIGN KEY (id_empreendimento)
+        REFERENCES licenciamento.empreendimento (id);
+
+GRANT USAGE ON SCHEMA analise TO tramitacao;
+GRANT SELECT ON ALL TABLES IN SCHEMA analise TO tramitacao;
+
+COMMIT;
+
+# --- !Downs
+
+BEGIN;
+
+ALTER TABLE licenciamento.empreendimento DROP COLUMN possui_anexo;
+
+ALTER TABLE analise.analise_geo_anexo DROP CONSTRAINT fk_aga_empreendimento;
+
+REVOKE USAGE ON SCHEMA analise FROM tramitacao;
+REVOKE SELECT ON ALL TABLES IN SCHEMA analise FROM tramitacao;
+
+COMMIT;
