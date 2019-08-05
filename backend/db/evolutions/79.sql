89e1eee08e9ad7bffec165e6a517d7b6fb18ff7a
Gabriel Oliveira <gabriel.oliveira.lemaf@gmail.com>
2019-08-05 11:22:22 -0300

    Adicionando entidade analise.desvinculo, para guardar as solicitacoes de desvinculo do analista GEO

--- backend/db/evolutions/79.sql
+++ backend/db/evolutions/79.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 
 ALTER TABLE analise.documento ADD COLUMN nome_arquivo TEXT;
-COMMENT ON COLUMN analise.documento.nome_arquivo IS 'Nome de referência para upload e dpwnload do arquivo armozenado na máquina';
+COMMENT ON COLUMN analise.documento.nome_arquivo IS 'Nome de referência para upload e download do arquivo armazenado na máquina';
 
 # --- !Downs
 
