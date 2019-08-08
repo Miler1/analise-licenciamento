2290a6e9677318eeff0d16fbaf96d10480f9648c
Gabriel Oliveira <gabriel.oliveira.lemaf@gmail.com>
2019-08-08 14:22:23 -0300

    Removendo coluna de status da tabela analise.processo

---
+++ backend/db/evolutions/82.sql
@@ -0,0 +1,18 @@
+# --- !Ups
+
+
+ALTER TABLE analise.processo DROP COLUMN status;
+
+# --- !Downs
+
+BEGIN;
+
+ALTER TABLE analise.processo ADD COLUMN status TEXT DEFAULT 'AGUARDANDO_ANALISE_GEO';
+
+COMMENT ON COLUMN analise.processo.status IS 'Coluna que guarda os possíveis status do processo (AGUARDANDO_ANALISE_GEO, SOLICITACAO_DESVINCULO_PENDENTE, NOTIFICACAO_ATENDIDA)';
+
+UPDATE analise.processo SET status = 'AGUARDANDO_ANALISE_GEO' WHERE status IS NULL;
+
+ALTER TABLE analise.processo ALTER COLUMN status SET NOT NULL;
+
+COMMIT;
