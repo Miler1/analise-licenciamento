cc940517469924b547d3a315f2d7dcc46b64455a
Gabriel Oliveira <gabriel.oliveira.lemaf@gmail.com>
2019-08-14 15:23:56 -0300

    Removendo dados desnecessários da external 41.sql

--- backend/db/externals/41.sql
+++ backend/db/externals/41.sql
@@ -4,15 +4,7 @@ INSERT INTO tramitacao.condicao(id_etapa, nm_condicao, fl_ativo) VALUES
 ((SELECT id_etapa FROM tramitacao.etapa where tx_etapa = 'Análise GEO'), 'Aguardando validação gerente', 1),
 ((SELECT id_etapa FROM tramitacao.etapa where tx_etapa = 'Análise GEO'), 'Aguardando validação pela diretoria', 1);
 
-
-INSERT INTO licenciamento.status_caracterizacao (id, nome, codigo) VALUES
-(15, 'Em análise', 'EM_ANALISE'),
-(16, 'Indeferido', 'INDEFERIDO'),
-(17, 'Solicitação de desvínculo', 'SOLICITACAO_DE_DESVINCULO');
-
 # --- !Downs
-
-DELETE FROM licenciamento.status_caracterizacao WHERE id BETWEEN  15 AND 17;
 
 DELETE FROM tramitacao.condicao
 WHERE id_etapa = (SELECT id_etapa FROM tramitacao.etapa WHERE tx_etapa = 'Análise GEO') AND
