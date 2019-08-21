1ea1ba909bdd4e1e4cf1871ccc324da36b26e931
Lucas.Pellaquim <lucaspellaquim.lp@gmail.com>
2019-08-21 14:18:29 -0300

    Ajustes inconsistencia

--- frontend/src/features/analiseGeo/analise-geo.controller.js
+++ frontend/src/features/analiseGeo/analise-geo.controller.js
@@ -415,12 +415,12 @@ var AnaliseGeoController = function($injector, $rootScope, $scope, $timeout, $ui
 		return inconsitenciaEncontrada !== undefined;
 	};
 
-	$scope.addInconsistenciaAtividade = function (categoriaInconsistencia, idAtividade , idGeometriaAtividade) {
+	$scope.addInconsistenciaAtividade = function (categoriaInconsistencia, idAtividadeCaracterizacao , idGeometriaAtividade) {
 
 		params = {
 			categoria:categoriaInconsistencia,
 			analiseGeo: {id: analiseGeo.id},
-			atividadeCaracterizacao: {id: idAtividade},
+			atividadeCaracterizacao: {id: idAtividadeCaracterizacao},
 			geometriaAtividade: {id: idGeometriaAtividade}			
 		};
 
@@ -441,6 +441,12 @@ var AnaliseGeoController = function($injector, $rootScope, $scope, $timeout, $ui
 					},
 					inconsistencia: function(){
 						return response.data;
+					},
+					idAtividadeCaracterizacao: function(){
+						return idAtividadeCaracterizacao;
+					},
+					idGeometriaAtividade: function(){
+						return idGeometriaAtividade;
 					}
 				}
 			});
@@ -489,6 +495,12 @@ var AnaliseGeoController = function($injector, $rootScope, $scope, $timeout, $ui
 						},
 						inconsistencia: function(){
 							return response.data;
+						},
+						idAtividadeCaracterizacao: function(){
+							return null;
+						},
+						idGeometriaAtividade: function(){
+							return null;
 						}
 					}
 				});
