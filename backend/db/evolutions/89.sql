3ca3e249f31fec0c7171673951a6dab980adab66
Gabriel Oliveira <gabriel.oliveira.lemaf@gmail.com>
2019-09-17 13:40:03 -0300

    Alteração da evolution 89.sql para novas conformidades

--- backend/db/evolutions/89.sql
+++ backend/db/evolutions/89.sql
@@ -1,5 +1,7 @@
 # --- !Ups
 
+BEGIN;
+
 ALTER TABLE analise.inconsistencia
     DROP CONSTRAINT fk_i_atividade_caracterizacao;
 
@@ -17,43 +19,49 @@ ALTER TABLE analise.inconsistencia
     ADD CONSTRAINT fk_i_caracterizacao FOREIGN KEY (id_caracterizacao)
         REFERENCES licenciamento.caracterizacao (id);
 
-ALTER TABLE analise.comunicado DROP COLUMN id_atividade_caracterizacao;
+ALTER TABLE analise.comunicado DROP CONSTRAINT fk_c_atividade_caracterizacao;
 
-CREATE TABLE analise.rel_comunicado_atividade_caracterizacao
-(
-    id_comunicado               INTEGER NOT NULL,
-    id_atividade_caracterizacao INTEGER NOT NULL,
+UPDATE analise.comunicado SET id_atividade_caracterizacao = ac.id_caracterizacao
+FROM (
+    SELECT a.id_caracterizacao, a.id FROM licenciamento.atividade_caracterizacao a
+        INNER JOIN analise.comunicado c ON a.id = c.id_atividade_caracterizacao
+    ) AS ac
+WHERE id_atividade_caracterizacao = ac.id;
 
-    CONSTRAINT fk_rcac_comunicado FOREIGN KEY (id_comunicado)
-        REFERENCES analise.comunicado (id)
-);
+ALTER TABLE analise.comunicado RENAME COLUMN id_atividade_caracterizacao TO id_caracterizacao;
+ALTER TABLE analise.comunicado
+    ADD CONSTRAINT fk_c_caracterizacao FOREIGN KEY (id_caracterizacao)
+        REFERENCES licenciamento.caracterizacao (id);
 
-ALTER TABLE analise.rel_comunicado_atividade_caracterizacao OWNER TO postgres;
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE analise.rel_comunicado_atividade_caracterizacao TO licenciamento_am;
+COMMENT ON COLUMN analise.comunicado.id_caracterizacao IS 'Coluna que relaciona um comunicado a uma caracterização';
 
-COMMENT ON TABLE analise.rel_comunicado_atividade_caracterizacao IS 'Entidade responsável pelo armazenamento das atividades do comunicado';
-COMMENT ON COLUMN analise.rel_comunicado_atividade_caracterizacao.id_comunicado IS 'Coluna que armazena o id do comunicado';
-COMMENT ON COLUMN analise.rel_comunicado_atividade_caracterizacao.id_atividade_caracterizacao IS 'Coluna que armazena o id da atividade de caracterização';
-
-ALTER TABLE analise.rel_comunicado_atividade_caracterizacao
-    ADD CONSTRAINT fk_rcac_atividade_caracterizacao FOREIGN KEY (id_atividade_caracterizacao)
-        REFERENCES licenciamento.atividade_caracterizacao (id);
+COMMIT;
 
 # --- !Downs
 
-DROP TABLE analise.rel_comunicado_atividade_caracterizacao;
+BEGIN;
 
+UPDATE analise.comunicado SET id_caracterizacao = NULL
+WHERE id_caracterizacao IS NOT NULL;
+
+ALTER TABLE analise.comunicado RENAME COLUMN id_caracterizacao TO id_atividade_caracterizacao;
 ALTER TABLE analise.comunicado
-    ADD COLUMN id_atividade_caracterizacao INTEGER,
+    DROP CONSTRAINT fk_c_caracterizacao,
     ADD CONSTRAINT fk_c_atividade_caracterizacao FOREIGN KEY (id_atividade_caracterizacao)
-        REFERENCES licenciamento.atividade_caracterizacao;
+        REFERENCES licenciamento.atividade_caracterizacao (id);
 
 COMMENT ON COLUMN analise.comunicado.id_atividade_caracterizacao IS 'Identificador da tabela atividade_caracterizacao, responsável pelo relacionamento entre as duas tabelas.';
 
-ALTER TABLE analise.inconsistencia RENAME COLUMN id_caracterizacao TO id_atividade_caracterizacao;
-COMMENT ON COLUMN analise.inconsistencia.id_atividade_caracterizacao IS 'Campo responsável por armazenar o id da atividade_caracterizacao da inconsistencia.';
 
+UPDATE analise.inconsistencia SET id_caracterizacao = NULL
+WHERE id_caracterizacao IS NOT NULL;
+
+ALTER TABLE analise.inconsistencia RENAME COLUMN id_caracterizacao TO id_atividade_caracterizacao;
 ALTER TABLE analise.inconsistencia
     DROP CONSTRAINT fk_i_caracterizacao,
     ADD CONSTRAINT fk_i_atividade_caracterizacao FOREIGN KEY (id_atividade_caracterizacao)
         REFERENCES licenciamento.atividade_caracterizacao (id);
+
+COMMENT ON COLUMN analise.inconsistencia.id_atividade_caracterizacao IS 'Campo responsável por armazenar o id da atividade_caracterizacao da inconsistencia.';
+
+COMMIT;
