610918788bd9fdabbcff556c99f5f538050e78bb
adriana.priscila <adriana.priscila.lemaf@gmail.com>
2019-11-27 07:49:53 -0300

    Ajustando permissões e comentários na evolution 103

--- backend/db/evolutions/102.sql
+++ backend/db/evolutions/102.sql
@@ -18,10 +18,10 @@ CREATE TABLE analise.parecer_gerente_analise_geo (
     CONSTRAINT fk_pgag_analise_geo FOREIGN KEY(id_analise_geo) 
         REFERENCES  analise.analise_geo(id)
 );
-GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE analise.parecer_gerente_analise_geo TO licenciamento_am;
-GRANT ALL ON TABLE analise.parecer_gerente_analise_geo TO postgres;
 ALTER TABLE analise.parecer_gerente_analise_geo OWNER TO postgres;
 ALTER TABLE analise.parecer_gerente_analise_geo OWNER TO licenciamento_am;
+GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE analise.parecer_gerente_analise_geo TO licenciamento_am;
+GRANT ALL ON TABLE analise.parecer_gerente_analise_geo TO postgres;
 COMMENT ON TABLE analise.parecer_gerente_analise_geo is 'Entidade responsável por armazenar informações sobre o parecer do gerente em uma análise técnica';
 COMMENT ON COLUMN analise.parecer_gerente_analise_geo.id is 'Id do parecer do gerente';
 COMMENT ON COLUMN analise.parecer_gerente_analise_geo.id_tipo_resultado_analise is 'Tipo do resultado da análise';
