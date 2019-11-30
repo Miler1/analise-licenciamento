01379cabb2288ef17e291c8537c18479c911f23c
adriana.priscila <adriana.priscila.lemaf@gmail.com>
2019-11-30 15:17:07 -0300

    Adicionando o campo analise.inconsistencia.id_atividade_caracterizacao

---
+++ backend/db/evolutions/106.sql
@@ -0,0 +1,9 @@
+
+# --- !Ups
+
+ALTER TABLE analise.inconsistencia ADD COLUMN id_atividade_caracterizacao INTEGER;
+COMMENT ON COLUMN analise.inconsistencia.id_atividade_caracterizacao IS 'Campo responsável por armazenar o id da atividade de caracterização quando a categoria é ATIVIDADE.';
+
+# --- !Downs
+
+ALTER TABLE analise.inconsistencia DROP COLUMN id_atividade_caracterizacao;
