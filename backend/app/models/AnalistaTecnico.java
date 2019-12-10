cf64de441f48ccdaf4ff4974d79d6808e7e32b48
Lucas.Pellaquim <lucaspellaquim.lp@gmail.com>
2019-12-10 15:26:33 -0300

    Sincronia de perfis

--- backend/app/models/AnalistaTecnico.java
+++ backend/app/models/AnalistaTecnico.java
@@ -66,8 +66,8 @@ public class AnalistaTecnico extends GenericModel {
 
 			Usuario usuario = usuariosEU.stream().filter(usuarioEU -> usuarioEU.login.equals(analistaTecnico.login)).findAny().orElseThrow(PortalSegurancaException::new);
 
-			usuario.salvarPerfis(analistaTecnico);
-			usuario.salvarSetores(analistaTecnico);
+			analistaTecnico.perfis = usuario.salvarPerfis(analistaTecnico);
+			analistaTecnico.setores = usuario.salvarSetores(analistaTecnico);
 
 		}
 
