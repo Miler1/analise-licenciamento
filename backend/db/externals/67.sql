d95f1fc9f8a133377e461797e8a594dd145e4847
adriana.priscila <adriana.priscila.lemaf@gmail.com>
2019-12-12 12:08:44 -0300

    Atribuindo permissao ao Gerente em portal_seguranca.permissao_perfil

---
+++ backend/db/externals/67.sql
@@ -0,0 +1,11 @@
+# --- !Ups
+
+INSERT INTO portal_seguranca.permissao_perfil(id_perfil, id_permissao)
+	VALUES ((SELECT id FROM portal_seguranca.perfil WHERE nome = 'Gerente' and id_modulo_pertencente = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')), (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'ANL_VISUALIZAR_NOTIFICACAO' AND id_modulo = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')));
+
+
+# --- !Downs
+
+DELETE FROM portal_seguranca.permissao_perfil 
+WHERE id_perfil = (SELECT id FROM portal_seguranca.perfil WHERE nome = 'Gerente' and id_modulo_pertencente = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')) 
+AND id_permissao in (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'ANL_VISUALIZAR_NOTIFICACAO');
