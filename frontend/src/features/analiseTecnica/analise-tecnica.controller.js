d0a566423abee1ad5fa8d94f2f8e44ea313d6b34
Lucas.Pellaquim <lucaspellaquim.lp@gmail.com>
2020-01-14 11:14:04 -0300

    Correcao de validaçao de inconsistencia tecnica de documento administrativo

--- frontend/src/features/analiseTecnica/analise-tecnica.controller.js
+++ frontend/src/features/analiseTecnica/analise-tecnica.controller.js
@@ -318,7 +318,7 @@ var AnaliseTecnicaController = function ($rootScope, uploadService, $route, $sco
 
             _.forEach(inconsistenciaTecnica, function(inconsistencia){
                 
-                if(documento.id === inconsistencia.inconsistenciaTecnicaDocumentoAdministrativo.documentoAdministrativo.id ){           
+                if(documento.id === inconsistencia.inconsistenciaTecnicaDocumentoAdministrativo.documentoAdministrativo.documento.id ){           
                     ctrl.itemValidoLicenca.documentoAdministrativo[index] = true;
                     return false;
 
