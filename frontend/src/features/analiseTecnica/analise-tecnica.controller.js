9489d1df723f0615d60d7b1ba58080babb3dd1d7
Lucas.Pellaquim <lucaspellaquim.lp@gmail.com>
2020-01-15 09:07:24 -0300

    Resolvendo problema de listagem de inconsistencias ao emitir notificacao

--- frontend/src/features/analiseTecnica/analise-tecnica.controller.js
+++ frontend/src/features/analiseTecnica/analise-tecnica.controller.js
@@ -698,11 +698,11 @@ var AnaliseTecnicaController = function ($rootScope, uploadService, $route, $sco
 
             ctrl.tabAtiva = ctrl.tabAtiva + 1;
 
+            ctrl.findInconsistenciasParaConclusao();
+
         } else {
 
             mensagem.error("Preencha os campos obrigatórios para prosseguir com a análise.");
-
-            ctrl.findInconsistenciasParaConclusao();
 
         }
 
