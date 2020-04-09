afddc15169a677329181955f9f4ede00d0382fc3
adriana.priscila <adriana.priscila.lemaf@gmail.com>
2020-04-09 12:04:13 -0300

    Criação do esquema tramitacao e das tabelas.

--- backend/db/externals/47.sql
+++
@@ -1,8 +0,0 @@
-# --- !Ups
-
-ALTER TABLE analise.comunicado
-    ADD CONSTRAINT fk_c_orgao FOREIGN KEY (id_orgao) REFERENCES licenciamento.orgao (id);
-
-# --- !Downs
-
-ALTER TABLE analise.comunicado DROP CONSTRAINT fk_c_orgao;
--- backend/db/externals/74.sql
+++
@@ -1,9 +0,0 @@
-# --- !Ups
-
-UPDATE tramitacao.condicao SET id_etapa = 7 WHERE id_condicao = 6;
-
-# --- !Downs
-
-UPDATE tramitacao.condicao SET id_etapa = null WHERE id_condicao = 6;
-
-
