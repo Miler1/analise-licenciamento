89db916f15576c7e2a2df4e41e147d9ff2f13735
Lucas.Pellaquim <lucaspellaquim.lp@gmail.com>
2020-07-23 13:53:46 -0300

    Alteracao de deploy e conf

--- scripts/conf/runners.sh
+++ scripts/conf/runners.sh
@@ -1,7 +1,7 @@
 config_play_version='1.5.0'
 config_update_server_app=true
-config_server_ssh="deploy@runners.ti.lemaf.ufla.br"
-config_server_release_folder_path="/home/deploy"
+config_server_ssh="sysadmin@177.105.35.22"
+config_server_release_folder_path="/home/sysadmin"
 config_server_app_path="/var/play/amazonas/licenciamento_ambiental/analise/"
 config_server_app_stop_cmd="systemctl stop analise-licenciamento.service"
 config_server_app_start_cmd="systemctl start analise-licenciamento.service"
