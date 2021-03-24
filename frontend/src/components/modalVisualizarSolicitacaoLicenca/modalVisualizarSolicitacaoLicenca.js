96565182bc96c21cd3bf580ee4b7118d87810c1c
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2021-03-24 09:19:54 -0300

    correçao visualizar cpf/cnpj e empreendimento do protocolo

--- frontend/src/components/modalVisualizarSolicitacaoLicenca/modalVisualizarSolicitacaoLicenca.js
+++ frontend/src/components/modalVisualizarSolicitacaoLicenca/modalVisualizarSolicitacaoLicenca.js
@@ -16,13 +16,15 @@ var ModalVisualizarSolicitacaoLicenca = {
 
         ctrl.$onInit =  function() {
 
+            
+
             ctrl.parecerTecnico = ctrl.resolve.parecerTecnico;
             ctrl.dadosProcesso = ctrl.resolve.dadosProcesso;
             ctrl.analiseTecnica = ctrl.resolve.analiseTecnica;
             ctrl.processo = ctrl.resolve.processo;
             ctrl.possuiValidade = (ctrl.analiseTecnica.analise.processo.caracterizacao.atividadesCaracterizacao[0].atividade.tipologia.codigo === ctrl.tipologias.ID_AQUICULTURA) ? false : true;
             ctrl.tiposResultadoAnalise = app.utils.TiposResultadoAnalise;
-            ctrl.cpfCnpj = (ctrl.processo.empreendimento.pessoa.cpf === null || ctrl.processo.empreendimento.pessoa.cpf === undefined) ? ctrl.processo.empreendimento.pessoa.cnpj : ctrl.processo.empreendimento.pessoa.cpf;
+            ctrl.cpfCnpj = (ctrl.processo.empreendimento.cpfCnpj === null || ctrl.processo.empreendimento.cpfCnpj === undefined) ? ctrl.processo.empreendimento.cpfCnpj : ctrl.processo.empreendimento.cpfCnpj;
 
         };
 
