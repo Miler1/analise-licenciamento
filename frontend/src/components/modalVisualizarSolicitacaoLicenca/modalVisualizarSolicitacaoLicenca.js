f2d77317c34ccfd524e5475a177d99a6c0cafbcf
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2021-03-24 09:37:11 -0300

    remoçao de comparaçao desnecessaria

--- frontend/src/components/modalVisualizarSolicitacaoLicenca/modalVisualizarSolicitacaoLicenca.js
+++ frontend/src/components/modalVisualizarSolicitacaoLicenca/modalVisualizarSolicitacaoLicenca.js
@@ -24,7 +24,7 @@ var ModalVisualizarSolicitacaoLicenca = {
             ctrl.processo = ctrl.resolve.processo;
             ctrl.possuiValidade = (ctrl.analiseTecnica.analise.processo.caracterizacao.atividadesCaracterizacao[0].atividade.tipologia.codigo === ctrl.tipologias.ID_AQUICULTURA) ? false : true;
             ctrl.tiposResultadoAnalise = app.utils.TiposResultadoAnalise;
-            ctrl.cpfCnpj = (ctrl.processo.empreendimento.cpfCnpj === null || ctrl.processo.empreendimento.cpfCnpj === undefined) ? ctrl.processo.empreendimento.cpfCnpj : ctrl.processo.empreendimento.cpfCnpj;
+            ctrl.cpfCnpj = ctrl.processo.empreendimento.cpfCnpj;
 
         };
 
