69f19a05b9c400ec8ffa65b283381e3558cb6520
adriana.priscila <adriana.priscila.lemaf@gmail.com>
2021-06-14 13:31:50 -0300

    Inserindo dados em portal_seguranca.perfil e portal_seguranca.permissao_perfil.

--- backend/db/externals/05.sql
+++ backend/db/externals/05.sql
@@ -48,10 +48,18 @@ INSERT INTO portal_seguranca.permissao_perfil(id_perfil, id_permissao) VALUES
 ((SELECT id FROM portal_seguranca.perfil WHERE codigo = 'COORDENADOR_GEO' and id_modulo_pertencente = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')),
 (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'VISUALIZAR_PROTOCOLO' AND id_modulo = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')));
 
+INSERT INTO portal_seguranca.permissao_perfil(id_perfil, id_permissao) VALUES
+((SELECT id FROM portal_seguranca.perfil WHERE codigo = 'COORDENADOR_GEO' and id_modulo_pertencente = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')),
+(SELECT id FROM portal_seguranca.permissao WHERE codigo = 'ANL_CONSULTAR_LICENCAS_EMITIDAS' AND id_modulo = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')));
+
 
 # --- !Downs
 
 -- Permissões do coordenador geo
+DELETE FROM portal_seguranca.permissao_perfil WHERE 
+id_perfil = (SELECT id FROM portal_seguranca.perfil WHERE codigo = 'COORDENADOR_GEO' and id_modulo_pertencente = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')) AND 
+id_permissao = (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'ANL_CONSULTAR_LICENCAS_EMITIDAS' AND id_modulo = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL'));
+
 DELETE FROM portal_seguranca.permissao_perfil WHERE 
 id_perfil = (SELECT id FROM portal_seguranca.perfil WHERE codigo = 'COORDENADOR_GEO' and id_modulo_pertencente = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL')) AND 
 id_permissao = (SELECT id FROM portal_seguranca.permissao WHERE codigo = 'ANL_LISTAR_PROCESSO' AND id_modulo = (SELECT id FROM portal_seguranca.modulo WHERE sigla = 'MAL'));
