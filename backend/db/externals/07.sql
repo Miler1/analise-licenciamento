3ccae8f1594811de1f4f01744f846c9744aade61
adriana.priscila <adriana.priscila.lemaf@gmail.com>
2021-08-18 09:51:29 -0300

    Inserindo dados em tramitacao.etapa. Alterando dados em tramitacao.condicao.

---
+++ backend/db/externals/07.sql
@@ -0,0 +1,12 @@
+# --- !Ups
+
+INSERT INTO tramitacao.etapa (id_etapa, id_fluxo, tx_etapa, dt_prazo) VALUES (8, 1, 'Pendência de vistoria', NULL);
+
+UPDATE tramitacao.condicao SET id_etapa = 8 WHERE id_condicao = 43;
+
+
+# --- !Downs
+
+DELETE FROM tramitacao.etapa WHERE id_etapa = 8 AND id_fluxo = 1;
+
+UPDATE tramitacao.condicao SET id_etapa = 8 WHERE id_condicao = 43;
